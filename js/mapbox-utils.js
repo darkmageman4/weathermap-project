73944a425b80c6d3a220de6c2cc9d9c023304fc4
darkmageman4 <darkmageman4@yahoo.com>
2021-06-11 16:10:57 -0400

    weather map

--- js/mapbox-utils.js
+++ js/mapbox-utils.js
@@ -1,25 +1,45 @@
-const token = "pk.eyJ1IjoiZGFya21hZ2VtYW40IiwiYSI6ImNrcHBtdzM2OTAya3oycXB0NjUyd2FwbGQifQ.ZxkFur5nI3aJZ19VNyDMFA"
-
 mapboxgl.accessToken = token;
 
 var map = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/mapbox/outdoors-v11",
     center: [-80.1918, 25.7617],
-    //[-80.1918, 25.7617],
     zoom: 11,
 })
 
-$.ajax({
-    url: "http://api.openweathermap.org/data/2.5/weather",
-    type: "GET",
-    data: {
-        // APPID: OPEN_WEATHER_MAP_TOKEN,
-        // lat:    29.423017,
-        // lon:   -98.48527,
-        // units: "imperial",
-    },
-    success: function (data){
-        console.log(data)
-    }
-});
+let geocoder = setGeoCoder();
+addGeocoderToMap(geocoder)
+addGeocoderEvent(event)
+
+function setGeoCoder() {
+    return new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+        marker: false
+    })
+};
+
+function addGeocoderToMap(geocoder) {
+    map.addControl(geocoder);
+}
+
+function addGeocoderEvent() {
+    geocoder.on("result", function (event) {
+        marker.setLngLat(event.result.geometry.coordinates)
+    })
+}
+
+// creates marker
+function setMarker(point) {
+    return new mapboxgl.Marker().setLngLat(point).addTo(map);
+}
+
+// add event to map that changes location based on user click
+function addMapEvent() {
+    map.on("click", function (e) {
+        console.log(e.lngLat)
+        marker.setLngLat(e.lngLat).addTo(map);
+    })
+}
+
+
