e02e3bd910d88c19ca222e1670dba900decec234
darkmageman4 <darkmageman4@yahoo.com>
2021-06-11 18:10:43 -0400

    weather map

--- js/mapbox-utils.js
+++ js/mapbox-utils.js
@@ -7,9 +7,22 @@ var map = new mapboxgl.Map({
     zoom: 11,
 })
 
+//call function to give method and initial pointer
+let marker = setMarker(
+    //[-80.1918, 25.7617]);
+//  Boobs
+    [-80.24007381597617, 26.155512538141295]
+)
+
+// call addMapEvent after marker has been initially set
+addMapEvent(marker)
+
+//create new geocoder
 let geocoder = setGeoCoder();
 addGeocoderToMap(geocoder)
 addGeocoderEvent(event)
+
+// setPopUp()
 
 function setGeoCoder() {
     return new MapboxGeocoder({
@@ -19,13 +32,14 @@ function setGeoCoder() {
     })
 };
 
+//add geocoder
 function addGeocoderToMap(geocoder) {
     map.addControl(geocoder);
 }
 
 function addGeocoderEvent() {
     geocoder.on("result", function (event) {
-        marker.setLngLat(event.result.geometry.coordinates)
+        marker(event.result.geometry.coordinates).setLngLat(event.result.geometry.coordinates).setPopup(getPopup(event.result.place_name));
     })
 }
 
@@ -37,9 +51,24 @@ function setMarker(point) {
 // add event to map that changes location based on user click
 function addMapEvent() {
     map.on("click", function (e) {
-        console.log(e.lngLat)
+        // console.log(e.lngLat)
         marker.setLngLat(e.lngLat).addTo(map);
     })
 }
 
+function setPopUp(textDetails) {
+    let popup = new mapboxgl.Popup().setHTML(`<p>${textDetails}</p>`)
+        .addTo(map);
+    marker.setPopup(popup);
+}
 
+function getPopup(textDetails) {
+    return new mapboxgl.Popup().setHTML(`<p>${textDetails}</p>`).addTo(map);
+}
+
+function addGeocoderEvent() {
+    geocoder.on("result", function (event) {
+        console.log(event);
+        marker.setLngLat(event.result.geometry.coordinates)
+    })
+}
