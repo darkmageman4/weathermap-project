1e02fbb36a104c1ce99f310c69223b760897f833
darkmageman4 <darkmageman4@yahoo.com>
2021-06-12 13:10:27 -0400

    weather map got weather request

--- js/mapbox-utils.js
+++ js/mapbox-utils.js
@@ -1,3 +1,6 @@
+mapboxgl.accessToken = token;
+
+
 var map = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/mapbox/outdoors-v11",
@@ -6,70 +9,63 @@ var map = new mapboxgl.Map({
 })
 
 //call function to give method and initial pointer
+
 let marker = setMarker(
-    //[-80.1918, 25.7617]);
-//  Boobs
     [-80.24007381597617, 26.155512538141295]
 )
-
 // call addMapEvent after marker has been initially set
-addMapEvent(marker)
 
+addMapEvent(marker)
 //create new geocoder
+
 let geocoder = setGeoCoder();
 addGeocoderToMap(geocoder)
 addGeocoderEvent(event)
-
+setPopUp()
 function setGeoCoder() {
+
     return new MapboxGeocoder({
         accessToken: mapboxgl.accessToken,
         mapboxgl: mapboxgl,
         marker: false
     })
 };
+//add geocoder
 
 function addGeocoderToMap(geocoder) {
     map.addControl(geocoder);
 }
-
 function addGeocoderEvent() {
+
     geocoder.on("result", function (event) {
-        marker.setLngLat(event.result.geometry.coordinates)
+        // marker.setLngLat(event.result.geometry.coordinates)
+                marker.setLngLat(event.result.geometry.coordinates).setPopup(getPopup(event.result.place_name));
+
     })
 }
-
 // creates marker
+
 function setMarker(point) {
     return new mapboxgl.Marker().setLngLat(point).addTo(map);
 }
-
 // add event to map that changes location based on user click
+
 function addMapEvent() {
     map.on("click", function (e) {
         console.log(e.lngLat)
         marker.setLngLat(e.lngLat).addTo(map);
     })
 }
+function setPopUp(textDetails) {
 
-$(document).ready(function () {
-    function getCityWeather(cityCoordinates) {
-        $.get("https://api.darksky.net/forecast/67b712ca42b8a1c502ad5da0ab0c40e3/"+ cityCoordinates + "?exclude=minutely,hourly,daily,alerts,flags", function(jsonData){
-            console.log(jsonData.currently);
-            $("#location").text(JSON.stringify(jsonData.currently));
-        }, "jsonp");
-    }
+    let popup = new mapboxgl.Popup().setHTML(`<p>${textDetails}</p>`)
+        .addTo(map);
+    marker.setPopup(popup);
+}
+function getPopup(textDetails) {
 
-    function getWeather(){
-        $.getJSON("httpcs://ipapi.co/json/", function(jsonData){
-            // console.log(jsonData);
-            var cityCoordinates = jsonData.latitude +","+ jsonData.longitude;
-            // console.log(cityCoordinates);
-            getCityWeather(cityCoordinates);
-        });
-    }
-    getWeather();
-});
-mapboxgl.accessToken = token;
+    return new mapboxgl.Popup().setHTML(`<p>${textDetails}</p>`).addTo(map);
+}
 
 
 
@@ -77,12 +73,7 @@ mapboxgl.accessToken = token;
 
 
 
-
-
-
-
-
-//
+///////                 Below if fixed code             !!!!!!!!!!!
 // var map = new mapboxgl.Map({
 //     container: "map",
 //     style: "mapbox://styles/mapbox/outdoors-v11",
@@ -126,6 +117,7 @@ mapboxgl.accessToken = token;
 //     })
 // }
 //
+//
 // // creates marker
 // function setMarker(point) {
 //     return new mapboxgl.Marker().setLngLat(point).addTo(map);
@@ -156,22 +148,5 @@ mapboxgl.accessToken = token;
 // function getPopup(textDetails) {
 //     return new mapboxgl.Popup().setHTML(`<p>${textDetails}</p>`).addTo(map);
 // }
-//
-// $(document).ready(function () {
-//     function getCityWeather(cityCoordinates) {
-//         $.get("https://api.darksky.net/forecast/67b712ca42b8a1c502ad5da0ab0c40e3/"+ cityCoordinates + "?exclude=minutely,hourly,daily,alerts,flags", function(jsonData){
-//             console.log(jsonData.currently);
-//             $("#location").text(JSON.stringify(jsonData.currently));
-//         }, "jsonp");
-//     }
-//
-// function getWeather(){
-//     $.getJSON("httpcs://ipapi.co/json/", function(jsonData){
-//         // console.log(jsonData);
-//         var cityCoordinates = jsonData.latitude +","+ jsonData.longitude;
-//         // console.log(cityCoordinates);
-//         getCityWeather(cityCoordinates);
-//     });
-// }
-// getWeather();
-// });
+
+// (function () {
