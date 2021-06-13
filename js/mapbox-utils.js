b6f28bc4bee141fc609c461d4e172ae71b9b2fe9
darkmageman4 <darkmageman4@yahoo.com>
2021-06-13 13:47:35 -0400

    weathermap-utils

--- js/mapbox-utils.js
+++ js/mapbox-utils.js
@@ -1,6 +1,5 @@
 mapboxgl.accessToken = token;
 
-
 var map = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/mapbox/outdoors-v11",
@@ -8,69 +7,63 @@ var map = new mapboxgl.Map({
     zoom: 11,
 })
 
-//call function to give method and initial pointer
-
-let marker = setMarker(
-    [-80.24007381597617, 26.155512538141295]
-)
-// call addMapEvent after marker has been initially set
-
-addMapEvent(marker)
 //create new geocoder
 
 let geocoder = setGeoCoder();
-addGeocoderToMap(geocoder)
-addGeocoderEvent(event)
-setPopUp()
-
 function setGeoCoder() {
-
     return new MapboxGeocoder({
         accessToken: mapboxgl.accessToken,
         mapboxgl: mapboxgl,
-        marker: false
+        marker: false //reset marker
     })
 };
 
+// inputBox(geocoder) //function for functionally inputBox
 //add geocoder
+// function inputBox(geocoder) {
+//     map.addControl(geocoder);//add geocoder to map
+// }
 
-function addGeocoderToMap(geocoder) {
-    map.addControl(geocoder);
-}
-
-function addGeocoderEvent() {
-
+resultsFromGeocoder(event)
+function resultsFromGeocoder() {
+    map.addControl(geocoder);//add geocoder to map
     geocoder.on("result", function (event) {
-        // marker.setLngLat(event.result.geometry.coordinates)
-        marker.setLngLat(event.result.geometry.coordinates).setPopup(getPopup(event.result.place_name));
-
+        console.log(event.)
+        marker.setLngLat(event.result.geometry.coordinates).setPopup(createsPopup(event.result.place_name));
+        console.log(marker)
     })
 }
 
-// creates marker
 
-function setMarker(point) {
+//should I name coordinate variable
+function geoCoderEventOnResult (geoCoderResult) {
+    geoCoderResult.on('result',function (data){
+        let coordinates = data.result.geometry.coordinates
+        marker.setLngLat(coordinates)
+            .addTo(map);
+        weatherMapUtils(coordinates);
+    })
+}
+
+//call function to give method and initial pointer (Miami)
+let marker = markerCreator([-80.24007381597617, 26.155512538141295])
+
+// creates marker
+function markerCreator(point) {
     return new mapboxgl.Marker().setLngLat(point).addTo(map);
 }
 
+// call clickFunction after marker has been initially set
+whatHappensWhenMapIsClicked(marker)
 // add event to map that changes location based on user click
-
-function addMapEvent() {
+function whatHappensWhenMapIsClicked() {
     map.on("click", function (e) {
         console.log(e.lngLat)
         marker.setLngLat(e.lngLat).addTo(map);
     })
 }
 
-function setPopUp(textDetails) {
-
-    let popup = new mapboxgl.Popup().setHTML(`<p>${textDetails}</p>`)
-        .addTo(map);
-    marker.setPopup(popup);
-}
-
-function getPopup(textDetails) {
-
+function createsPopup(textDetails) {
     return new mapboxgl.Popup().setHTML(`<p>${textDetails}</p>`).addTo(map);
 }
 
@@ -154,3 +147,13 @@ function getPopup(textDetails) {
 // }
 
 // (function () {
+
+
+/////  Do Not Need   ????????
+
+// setPopUp()
+// function setPopUp(textDetails) {
+//     let popup = new mapboxgl.Popup().setHTML(`<p>${textDetails}</p>`)
+//         .addTo(map);
+//     marker.setPopup(popup);
+// }
--- js/weathermap-utils.js
+++ js/weathermap-utils.js
@@ -1,13 +1,19 @@
-$.ajax({
-    url: "http://api.openweathermap.org/data/2.5/weather",
-    type: "GET",
-    data: {
-        APPID: OPEN_WEATHER_MAP_TOKEN,
-        lat: 25.7617,
-        lon: -80.1918,
-        units: "imperial",
-    },
-    success: function (data) {
-        console.log('5 day forecast', data);
-    }
-});
+// mapboxCoordinates()
+// function mapboxCoordinates () {
+
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather",
+        type: "GET",
+        data: {
+            APPID: OPEN_WEATHER_MAP_TOKEN,
+            lat: 25.7617,
+            lon: -80.1918,
+            units: "imperial",
+        },
+        success: function (data) {
+            console.log('5 day forecast', data);
+        }
+    });
+
+// }
+
